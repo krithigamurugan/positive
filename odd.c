ebde9046a696da53fbdb4935625a91f959ec5811
krithigamurugan <krithigamurugan263@gmail.com>
2016-06-24 21:02:02 +0530

    Update odd.c

--- odd.c
+++ odd.c
@@ -1,15 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-    int m;
-
-    printf("Enter an integer: ");
-    scanf("%d",&m);
+int m;
+printf("Enter an integer: ");
+scanf("%d",&m);
 if(m%2 == 0)
-        printf("%d is even.",m);
-    else
-        printf("%d is odd.",m);
-
-    return 0;
+printf("%d is even.",m);
+else
+printf("%d is odd.",m);
+return 0;
 }
 
